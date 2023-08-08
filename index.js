326af03b8595a5182385a442d1875adafdc49d5a
Tiana Lopez <tianadavin@gmail.com>
2023-08-08 11:48:00 -0700

    Adds functions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {name: "Tian", address: "1234 Jones"};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const update = {...employee};
+  update[key] = value;
+  return update;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
