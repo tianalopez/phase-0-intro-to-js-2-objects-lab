11e04bcae8d8ae00788ea58d07664b5aa4ed6be4
Tiana Lopez <tianadavin@gmail.com>
2023-09-12 10:54:20 -0700

    Redo problems

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-// Write your solution in this file!
+// // Write your solution in this file!
 const employee = {name: "Tian", address: "1234 Jones"};
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//   const update = {...employee};
+//   update[key] = value;
+//   return update;
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//   employee[key] = value;
+//   return employee;
+// }
+
+// function deleteFromEmployeeByKey(employee, key) {
+//   const newEmployee = { ...employee };
+//   delete newEmployee[key];
+//   return newEmployee;
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//   delete employee[key];
+//   return employee;
+// }
+
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-  const update = {...employee};
-  update[key] = value;
-  return update;
+  const newObj = {...employee};
+  newObj[key] = value;
+  return newObj;
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
@@ -12,9 +35,9 @@ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
 }
 
 function deleteFromEmployeeByKey(employee, key) {
-  const newEmployee = { ...employee };
-  delete newEmployee[key];
-  return newEmployee;
+  const newObj = {...employee};
+  delete newObj[key];
+  return newObj;
 }
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
